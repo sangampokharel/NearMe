eb4b3d41e70e2c252dc8cb653788ecfd64b5dd87
sangam <sangampokharel803@gmail.com>
2025-05-13 14:28:48 +0545

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by EKbana on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
